1435834784944c0362f88d8a6c7f33fec249c72d
KyhEN <91900788+KyhEN@users.noreply.github.com>
2022-12-10 23:50:20 -0500

    Create SetObject.java

---
+++ Group/src/com/main/SetObject.java
@@ -0,0 +1,21 @@
+public class Setter {
+	Panel p;
+	
+	public 	Setter(Panel p) {
+		this.p = p;
+	}
+	
+	public void setObject() {
+		p.o[0] = new Obj_Key();
+		p.o[0].worldx = 20 * p.tSize;
+		p.o[0].worldy = 12 * p.tSize;
+		
+		p.o[1] = new Obj_Door2();
+		p.o[1].worldx = 1* p.tSize;
+		p.o[1].worldy = 2 * p.tSize;
+		
+		p.o[2] = new Obj_Coin();
+		p.o[2].worldx = 1 * p.tSize;
+		p.o[2].worldy = 2 * p.tSize;
+	}
+}
