a579ac4b065c92f60d17f1eb5f98d6b23adf599b
KyhEN <91900788+KyhEN@users.noreply.github.com>
2022-12-10 23:52:32 -0500

    Update SetObject.java

--- Group/src/com/main/SetObject.java
+++ Group/src/com/main/SetObject.java
@@ -6,14 +6,6 @@ public class SetObj {
 	}
 	
 	public void setObject() {
-		p.o[0] = new Obj_Key();
-		p.o[0].worldx = 20 * p.tSize;
-		p.o[0].worldy = 12 * p.tSize;
-		
-		p.o[1] = new Obj_Door2();
-		p.o[1].worldx = 1* p.tSize;
-		p.o[1].worldy = 2 * p.tSize;
-		
 		p.o[2] = new Obj_Coin();
 		p.o[2].worldx = 1 * p.tSize;
 		p.o[2].worldy = 2 * p.tSize;
