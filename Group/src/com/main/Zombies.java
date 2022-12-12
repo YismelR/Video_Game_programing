c024ef22cac76a1dc560dc34bfe7f5d9027056d9
KyhEN <91900788+KyhEN@users.noreply.github.com>
2022-12-11 19:13:53 -0500

    Update Zombies.java

--- Group/src/com/main/Zombies.java
+++ Group/src/com/main/Zombies.java
@@ -2,64 +2,51 @@ import java.awt.Graphics;
 import java.util.Random;
 
 public class Zombies extends Soldier{
-	private static final int WALK = 0;
-	private static final int ATTACK = 0;
-	private static final int DEAD = 0;
-	int pose = WALK;
-	
 	int vx;
 	int vy;
 	
 	int ax; 
 	int ay;
 	
-	Soldier soldier;
 	Animation [] anim;
 	
+	static final int WALK = 0;
+	static final int DEAD = 1;
+	static final int ATTACK = 2;
+	
+	int pose = ATTACK;
+
 	int health = 5;
+	
+	Soldier s;
 	
 	public Zombies(int x, int y, int w, int h) {
 		super(x, y, w, h);
+
+		anim = new Animation[3];
 		
-		String [] pose = {"walk"};
+		String [] pose = {"walk", "dead", "attack"};
 		
 		for(int i = 0; i < anim.length; i++) {
-			anim[i] = new Animation("fz_" + pose[i] + "_", 10, ".GIF", 15);
-		}
-	}
-	
-	public void chase(Soldier soldier) {
-		int xdiff = Math.abs(x - soldier.x) ; 
-		int ydiff = Math.abs(y - soldier.y) ; 
-		
-		if(xdiff < 200) {
-			if(xdiff > 10) {
-	      		if (x < soldier.x)   moveRight(4);
-	   		}
+			anim[i] = new Animation("femalez-" + pose[i] + "-", 12, ".GIF", 10);
 		}
 	}
 	
 	public boolean dead(){
-		if(health <= 0) {
+		if(health < 0) {
 			return true;
 		}
 		return false;
 	}
 	
 	public boolean attack(){
-		if(x <= soldier.x) {
+		if(s.x <= 10) {
 			return true;
 		}
 		return false;
 	}
 	
 	public void draw(Graphics g) {
-		if(!(dead())) {
-			g.drawImage(anim[pose].currentImage(), x - Camera.x, y - Camera.y, w/2, h/2, null);
-			if(attack()) {
-				g.drawImage(anim[ATTACK].currentImage(), x - Camera.x, y - Camera.y, w/2, h/2, null);
-			}
-		}
-		if(dead()) g.drawImage(anim[DEAD].currentImage(), x - Camera.x, y - Camera.y, w/2, h/2, null);
+		g.drawImage(anim[pose].currentImage(), x - Camera.x, y - Camera.y, w/2, h/2, null);
 	}
 }
