78e1b08b0159c73457649ec48f59f0a780854d27
KyhEN <91900788+KyhEN@users.noreply.github.com>
2022-12-13 17:44:23 -0500

    Update Zombies.java

--- Test/src/com/main/Zombies.java
+++ Test/src/com/main/Zombies.java
@@ -15,7 +15,7 @@ public class Zombies extends Soldier{
 	static final int DEAD = 1;
 	static final int ATTACK = 2;
 	
-	int pose = ATTACK;
+	int pose = WALK;
 
 	int health = 5;
 	
@@ -37,12 +37,6 @@ public class Zombies extends Soldier{
 	//but, because neither zombies.java or soldier.java extends gamebase,
 	//which contains "health_status", it wont work here. Rather, create this
 	//constructor in GameF22 instead for better results
-	public boolean dead(){
-		if(health == 0) {
-			return true;
-		}
-		return false;
-	}
 	
 	public boolean attack(){
 		if(s.x < 10) {
