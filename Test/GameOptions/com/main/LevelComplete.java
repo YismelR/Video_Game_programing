f027f2dc766f25b3cbde300f548a6f7108ccf11f
JoeDeb <113562378+JoeDebrah@users.noreply.github.com>
2022-12-16 12:58:57 -0500

    Updated LevelComplete with  JButtons

--- Test/GameOptions/com/main/LevelComplete.java
+++ Test/GameOptions/com/main/LevelComplete.java
@@ -1,14 +1,13 @@
 package com.main;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.Image;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
 
 public class LevelComplete extends GameBase implements MouseListener{
 	
@@ -16,26 +15,41 @@ public class LevelComplete extends GameBase implements MouseListener{
 	boolean reset = false;
 	boolean level = false;
 	
-	BufferedImage img;
-	int imgW; 
-	int imgH;
-	int imgX;
-	int imgY;
+	//button play
+	private Rectangle playAgainBtn;
+	private String playAgainTxt = "Play Again";
+	//button next level
+	private Rectangle nextLevelBtn;
+	private String nextLevelTxt = "Next Level";
+	//button main menu
+	private Rectangle mainBtn;
+	private String mainTxt = "Main Menu";
+	//quit
+	private Rectangle quitBtn;
+	private String quitTxt = "Quit";
 	
-	public Image background() {
-       try {
-           img = ImageIO.read(new File("Level Complete.png"));
-       } catch (IOException e) { }
- 
-        imgW = img.getWidth(null);
-        imgH = img.getHeight(null);
-        imgX = (width/2 - imgW/2);
-    	imgY = (height/2 - imgH/2);
-        return img;
-	}
+	private Rectangle background;
+	
+	private Font font;
 	
 	public LevelComplete(GameBase game) {
 	active = false;
+	
+	int BackW = 500; 
+	int BackH = 500;
+	int BackX = (width/2 - BackW/2);
+ 	int BackY = (height/2 - BackH/2);
+	int w, h;
+	w = 300;
+	h = 100;
+	
+	background  = new Rectangle(BackX, BackY, BackW, BackH);
+	playAgainBtn = new Rectangle(BackX + BackW/5, BackY + 50, w, h);
+	nextLevelBtn = new Rectangle(BackX + BackW/5, BackY + 150, w, h);
+	mainBtn = new Rectangle(BackX + BackW/5, BackY + 250, w, h);	
+	quitBtn = new Rectangle(BackX + BackW/5, BackY + 350, w, h);	
+	font  = new Font("Roboto", Font.BOLD, 50);
+	
 	}
 	
 	public void levelcompleted() {
@@ -43,38 +57,66 @@ public class LevelComplete extends GameBase implements MouseListener{
 	}
 		
 	public void draw(Graphics g) {
-		g.drawImage(background(), imgX, imgY, imgW, imgH, null);
+		
+		Graphics2D g2d = (Graphics2D) g;
+		
+		g.setFont(font);
+		g.setColor(Color.black) ;
+		g2d.fill(playAgainBtn) ; 
+		
+		g.setColor(Color.black) ;
+		g2d.fill(quitBtn); 
+		
+		g.setColor(Color.black) ;
+		g2d.fill(mainBtn); 
+		
+		g.setColor(Color.black) ;
+		g2d.fill(nextLevelBtn); 
+	
+		g.setColor(Color.black) ;
+		g2d.fill(background);
+		
+		g.setColor(Color.white) ;
+		g2d.draw(background);
+		g2d.draw(playAgainBtn);
+		g2d.draw(nextLevelBtn);
+		g2d.draw(quitBtn);
+		g2d.draw(mainBtn);
+
+		int strW = g.getFontMetrics(font).stringWidth(playAgainTxt);
+		int strH = g.getFontMetrics(font).getHeight();
+		g.setColor(Color.green);
+		g.drawString(playAgainTxt, playAgainBtn.x + (int)(playAgainBtn.getWidth()/8) , playAgainBtn.y + (int)(playAgainBtn.getHeight()/2 + 30));
+		
+		strW = g.getFontMetrics(font).stringWidth(quitTxt);
+		strH = g.getFontMetrics(font).getHeight();
+		g.setColor(Color.red);
+		g.drawString(quitTxt, quitBtn.x + (int)(quitBtn.getWidth()/4) , quitBtn.y + (int)(quitBtn.getHeight()/2 + 30));	
+		
+		strW = g.getFontMetrics(font).stringWidth(mainTxt);
+		strH = g.getFontMetrics(font).getHeight();
+		g.setColor(Color.blue);
+		g.drawString(mainTxt, mainBtn.x + (int)(mainBtn.getWidth()/8) , mainBtn.y + (int)(mainBtn.getHeight()/2 + 30));	
+		
+		strW = g.getFontMetrics(font).stringWidth(nextLevelTxt);
+		strH = g.getFontMetrics(font).getHeight();
+		g.setColor(Color.blue);
+		g.drawString(nextLevelTxt, nextLevelBtn.x + (int)(mainBtn.getWidth()/8) , nextLevelBtn.y + (int)(nextLevelBtn.getHeight()/2 + 30));	
+		
 	}
 
 	public void mouseMoved(MouseEvent e) { }
 
 	public void mouseClicked(MouseEvent e) {
-		int MouseX=e.getX();
-		int MouseY=e.getY();
 		
+		Point p =  e.getPoint ();
 		if(active) {
-		//From level complete screen go to Main menu
-		if(MouseX >= imgX+(imgW/4) && MouseX <= imgX + (imgW/4)+imgW/2 && MouseY >= imgY + (imgH/8 *4)-8 && MouseY <= imgY + (imgH/8 *4)-8 +imgH/8) { levelNextLevel=true; } 
-		else { levelNextLevel=false; } 
-	
-		//From level complete screen play again
-		if(MouseX >= imgX+(imgW/4) && MouseX <= imgX+(imgW/4)+imgW/2 && MouseY >= imgY + (imgH/8 * 5)-6 && MouseY <= imgY + (imgH/8 * 5)-6 +imgH/8) { levelPlayAgain=true; } 
-		else { levelPlayAgain=false; } 
-		
-		//From level complete screen Quit
-		if(MouseX >= imgX+(imgW/4) && MouseX <= imgX+(imgW/4)+imgW/2 && MouseY >= imgY +(imgH/8 *6)-4 && MouseY <= imgY +(imgH/8 *6)-4 +imgH/8) { levelMainMenu=true; } 
-		else { levelMainMenu=false; }
-		
-		//if below level 6, from level complete screen, go to next level
-		if(MouseX >= imgX+(imgW/4) && MouseX <= imgX+(imgW/4)+imgW/2 && MouseY >= imgY +(imgH/8 *7) && MouseY <= imgY +(imgH/8 *7)+imgH/8) { levelQuitGame=true; } 
-		else { levelQuitGame=false; } 
-			
-		if(levelPlayAgain) {
+		if(playAgainBtn.contains(p)) {
 		active = false;
 		playAgain();
 		System.out.println("levelcomplete playAgain"); }
 		
-		if(levelNextLevel) {
+		if(nextLevelBtn.contains(p)) {
 		active=false;
 		levelComplete();
 		NextLevel();
@@ -82,13 +124,13 @@ public class LevelComplete extends GameBase implements MouseListener{
 		System.out.println("levelcomplete next level"); }
 		
 		//go to main menu from the level complete screen
-		if(levelMainMenu) {
+		if(mainBtn.contains(p)) {
 		active=false;
 		mainmenu.active=true;
 		GoToMainMenu();
 		System.out.println("levelcomplete main menu"); }
 		
-		if(levelQuitGame) {
+		if(quitBtn.contains(p)) {
 		ExitGame();
 		System.out.println("levelcomplete exit Game"); }
 		}
