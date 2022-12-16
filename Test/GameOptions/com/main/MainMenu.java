555dff0291ea1c3fcedc4ac04f10958a76724006
JoeDeb <113562378+JoeDebrah@users.noreply.github.com>
2022-12-16 13:00:32 -0500

    Updated mainmenu with Jbuttons

--- Test/GameOptions/com/main/MainMenu.java
+++ Test/GameOptions/com/main/MainMenu.java
@@ -1,98 +1,110 @@
 package com.main;
+
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.Image;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
-import javax.imageio.ImageIO;
+public class MainMenu extends GameBase implements MouseListener   {
 
-public class MainMenu extends GameBase implements MouseListener {
+	public boolean active;
+	public boolean reset;
 	
-	boolean active = false;
-	boolean reset = false;
-
-	BufferedImage img; 
-	int imgW; 
-	int imgH;
-	int imgX;
-	int imgY;
+	//button play
+	private Rectangle playBtn;
+	private String playTxt = "Play";
+	//but ton play
+	private Rectangle quitBtn;
+	private String quitTxt = "Quit";
+	private String optionTxt = "Main Menu";
 	
-	public Image background() {
-       try {
-           img = ImageIO.read(new File("Main Menu.png"));
-       } catch (IOException e) { }
-       
-        imgW = img.getWidth(null);
-        imgH = img.getHeight(null); 
-        imgX = 0;
-    	imgY = 0;
-        return img;
-	}
-
-	public MainMenu(GameBase game) {
+	private Rectangle background;
+	
+	private Font font;
+	
+	public MainMenu (GameBase game) {
 	active = true;
-	mainmenu();
-	game.start();	
-	}
+	game.start ();
 	
-	public void draw(Graphics g) {
-		g.drawImage(background(), 0, 0, width, height, null);
-	}
+	int BackW = 500; 
+	int BackH = 500;
+	int BackX = (width/2 - BackW/2);
+ 	int BackY = (height/2 - BackH/2);
+	int w, h;
+	w = 300;
+	h = 150;
 	
-	public void mouseClicked(MouseEvent e) {
-		
-		int MouseX=e.getX();
-		int MouseY=e.getY();
-		
-		if(active) {
-		//From main menu start Game
-		if(MouseX >= imgX+200 && MouseX <= imgX+200 + imgW/8 && MouseY >= imgY + (imgH/6)+100 && MouseY <= imgY + (imgH/6)+100 + imgH/10) { menuStartGame=true; }
-		else  { menuStartGame=false; }	
-
-		//From main menu Credits
-		if(MouseX >= imgX+170 && MouseX <= imgX+170 + imgW/6 && MouseY >= imgY + (imgH/6 *2) +40 && MouseY <= imgY + (imgH/6 *2) + 40 + imgH/15) { menuCredits=true; }
-		else { menuCredits=false; }
-		
-        //From main menu quit Game
-		if(MouseX >= imgX+130 && MouseX <= imgX+130 + imgW/5 && MouseY >= imgY + (imgH/7 *3) +35 && MouseY <= imgY + (imgH/7 *3) + 35 + imgH/15) { menuQuitGame=true; }
-		else { menuQuitGame=false; }
-		
-		if (menuStartGame) {
-		active=false;
-		startGame();
-		reset=true;
-		System.out.println("Main menu StartGame");}
-		
-		if (menuQuitGame) {
-		ExitGame();
-		System.out.println("Main menu Quit");}
-		
-		else if(menuCredits) {
-		System.out.println("Main menu Credits");}
-		}
-
-		
-	}
-	
-	public void mouseMoved(MouseEvent e) { }
-
-	@Override
-	public void mousePressed(MouseEvent e) {  }
-
-	@Override
-	public void mouseReleased(MouseEvent e) {  }
-
-	@Override
-	public void mouseEntered(MouseEvent e) {  }
-
-	@Override
-	public void mouseExited(MouseEvent e) {	}
-
-	@Override
-	public void inGameLoop() {	}
-	
+	background  = new Rectangle(BackX, BackY, BackW, BackH);
+	playBtn = new Rectangle(BackX + BackW/5, BackY+ BackH/5, w, h);
+	quitBtn = new Rectangle(BackX + BackW/5, BackY + BackH/2, w, h);	
+	font  = new Font("Roboto", Font.BOLD, 80);
 
 }
+	
+	public void draw(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		
+		g.setFont(font);
+		g.setColor(Color.black) ;
+		g2d.fill(playBtn) ; 
+		
+		g.setColor(Color.black) ;
+		g2d.fill(quitBtn); 
+	
+		g.setColor(Color.black) ;
+		g2d.fill(background);
+		
+		g.setColor(Color.white) ;
+		g2d.draw(background);
+		g2d.draw(playBtn);
+		g2d.draw(quitBtn);
+
+		int strW = g.getFontMetrics(font).stringWidth(playTxt);
+		int strH = g.getFontMetrics(font).getHeight();
+		g.setColor(Color.green);
+		g.drawString(playTxt, playBtn.x + (int)(playBtn.getWidth()/4) , playBtn.y + (int)(playBtn.getHeight()/2 + 30));
+		
+		g.setColor(Color.ORANGE);
+		g.drawString(optionTxt, background.x + 50, background.y + 80);
+		
+		strW = g.getFontMetrics(font).stringWidth(quitTxt);
+		strH = g.getFontMetrics(font).getHeight();
+		g.setColor(Color.red);
+		g.drawString(quitTxt, quitBtn.x + (int)(quitBtn.getWidth()/4) , quitBtn.y + (int)(quitBtn.getHeight()/2 + 30));
+		
+	}
+
+	public void mouseClicked (MouseEvent e) {
+	Point p =  e.getPoint ();
+	if(active) {
+	if (playBtn.contains(p)) {
+	startGame();
+	reset = true;
+	active = false; 
+	System.out.println("MainMenu startGame");}
+	 
+	if(quitBtn.contains(p)) {
+	ExitGame();}
+	}
+	}
+	
+	public void mouseMoved(MouseEvent e) {	}
+
+	public void mouseReleased(MouseEvent e) {  }
+
+	public void mouseEntered(MouseEvent e) {  }
+
+	public void mouseExited(MouseEvent e) {  }
+
+	public void mousePressed(MouseEvent e) {  }
+
+	public void inGameLoop() {	}
+	
+	
+ }
+	
+	
