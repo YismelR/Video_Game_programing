2776e66745d71ca878f1f1d757a3cfccf1e20e24
JoeDeb <113562378+JoeDebrah@users.noreply.github.com>
2022-12-16 12:56:43 -0500

    Updated with JButtons

--- Test/GameOptions/com/main/GameComplete.java
+++ Test/GameOptions/com/main/GameComplete.java
@@ -1,78 +1,109 @@
 package com.main;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.Image;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
 
 public class GameComplete extends GameBase implements MouseListener{
 	
 	boolean active = false;
 	boolean reset = false;
 	
-	BufferedImage img;
-	int imgW; 
-	int imgH;
-	int imgX;
-	int imgY;
+	//button play
+	private Rectangle playAgainBtn;
+	private String playAgainTxt = "Play Again";
+	//button main menu
+	private Rectangle mainBtn;
+	private String mainTxt = "Main Menu";
+	//quit
+	private Rectangle quitBtn;
+	private String quitTxt = "Quit";
 	
-	public Image background() {
-       try {
-           img = ImageIO.read(new File("Game complete.png"));
-       } catch (IOException e) { }
- 
-        imgW = img.getWidth(null);
-        imgH = img.getHeight(null); 
-        imgX = (width/2 - imgW/2);
-    	imgY = (height/2 - imgH/2);
-        return img;
-	}
+	private Rectangle background;
+	
+	private Font font;
 
 	public GameComplete(GameBase game) {
 	active = false;
+	
+	int BackW = 500; 
+	int BackH = 500;
+	int BackX = (width/2 - BackW/2);
+ 	int BackY = (height/2 - BackH/2);
+	int w, h;
+	w = 300;
+	h = 100;
+	
+	background  = new Rectangle(BackX, BackY, BackW, BackH);
+	playAgainBtn = new Rectangle(BackX + BackW/5, BackY + 100, w, h);
+	mainBtn = new Rectangle(BackX + BackW/5, BackY + 200, w, h);	
+	quitBtn = new Rectangle(BackX + BackW/5, BackY + 300, w, h);	
+	font  = new Font("Roboto", Font.BOLD, 50);
 	}
 	
 	public void draw(Graphics g) {
-		g.drawImage(background(), imgX, imgY, imgW, imgH, null);
-	}
+		
+		Graphics2D g2d = (Graphics2D) g;
+		
+		g.setFont(font);
+		g.setColor(Color.black) ;
+		g2d.fill(playAgainBtn) ; 
+		
+		g.setColor(Color.black) ;
+		g2d.fill(quitBtn); 
+		
+		g.setColor(Color.black) ;
+		g2d.fill(mainBtn); 
+	
+		g.setColor(Color.black) ;
+		g2d.fill(background);
+		
+		g.setColor(Color.white) ;
+		g2d.draw(background);
+		g2d.draw(playAgainBtn);
+		g2d.draw(quitBtn);
+		g2d.draw(mainBtn);
+
+		int strW = g.getFontMetrics(font).stringWidth(playAgainTxt);
+		int strH = g.getFontMetrics(font).getHeight();
+		g.setColor(Color.green);
+		g.drawString(playAgainTxt, playAgainBtn.x + (int)(playAgainBtn.getWidth()/8) , playAgainBtn.y + (int)(playAgainBtn.getHeight()/2 + 30));
+		
+		strW = g.getFontMetrics(font).stringWidth(quitTxt);
+		strH = g.getFontMetrics(font).getHeight();
+		g.setColor(Color.red);
+		g.drawString(quitTxt, quitBtn.x + (int)(quitBtn.getWidth()/4) , quitBtn.y + (int)(quitBtn.getHeight()/2 + 30));	
+		
+		strW = g.getFontMetrics(font).stringWidth(mainTxt);
+		strH = g.getFontMetrics(font).getHeight();
+		g.setColor(Color.blue);
+		g.drawString(mainTxt, mainBtn.x + (int)(mainBtn.getWidth()/8) , mainBtn.y + (int)(mainBtn.getHeight()/2 + 30));	
+		
+		}
 
 
 	public void mouseMoved(MouseEvent e) { }
 
 	public void mouseClicked(MouseEvent e) {
-		int MouseX=e.getX();
-		int MouseY=e.getY();
-			
+		
+		Point p = e.getPoint ();	
 		if(active) {
-		//From game complete screen, go to main menu
-		if(MouseX >= imgX + (imgW/4)-25 && MouseX <= imgX + (imgW/4)-25+imgW/2+40 && MouseY >= imgY + (imgH/8 * 5)-35 && MouseY <= imgY + (imgH/8 * 5)-35 + imgH/8) { completePlayAgain=true; } 
-		else { completePlayAgain=false; } 
-		
-		//from game complete screen, play again
-		if(MouseX >= imgX + (imgW/4)-25 && MouseX <= imgX + (imgW/4)-25+imgW/2+40 && MouseY >= imgY +(imgH/8 *6)-20 && MouseY <= imgY +(imgH/8 *6)-20 + imgH/8) { completeMainMenu=true; } 
-		else { completeMainMenu=false;} 		
-		
-        //From game complete screen, quit Game
-		if(MouseX >= imgX + (imgW/4)-25 && MouseX <= imgX + (imgW/4)-25+imgW/2+40 && MouseY >= imgY +(imgH/8 *7)-6 && MouseY <= imgY +(imgH/8 *7)-6 + imgH/8) { completeQuit=true; }
-		else { completeQuit=false; }
-		
-		//Point p = e.getPoint ();
-		if (completePlayAgain) {
+		if (playAgainBtn.contains(p)) {
 		active = false; 
 		playAgain();
 		System.out.println("gamecomplete playagain");}
 		
-		if(completeMainMenu) {
+		if(mainBtn.contains(p)) {
 		active=false;
 		mainmenu.active=true;
 		GoToMainMenu();
 		System.out.println("gamecomplete mainmenu");}
 		
-		if (completeQuit) {
+		if (quitBtn.contains(p)) {
 		ExitGame();
 		System.out.println("gamecomplete quit");}
 		
